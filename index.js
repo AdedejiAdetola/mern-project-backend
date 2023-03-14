3dbfce365e6643915ad572891c5be547be20e6e7
unknown <adetolaadedeji0@gmail.com>
2023-03-14 12:13:30 +0100

    Update cors

--- index.js
+++ index.js
@@ -13,9 +13,15 @@ import userRoutes from './routes/users.js';
 
 const app = express() //initializing app
 
+
+const PORT = process.env.PORT;
 app.use(bodyParser.json({ limit: "30mb", extended: true })) //limit, as images will be passed to our app
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })) //setting up bodyparser to send requests
-app.use(cors());
+app.use(
+    cors({
+        origin: [`http://localhost:${PORT}`, "https://memories-mern-app.onrender.com"]
+    })
+);
 
 
 app.use('/posts', postRoutes);
@@ -24,7 +30,6 @@ app.use('/users', userRoutes);
 
 //connect to databse 
 const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //returns a promise
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err.message));
